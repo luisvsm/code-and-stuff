47be741045a7c336bdde3d34d3aed06d5b91d6dc
Luis Van Slageren <luisvsm@gmail.com>
2017-07-23 11:28:44 +1000

    Added boot controller and box spawn controller

---
+++ Unity/Code and stuff/Assets/Scripts/BootController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BootController : MonoBehaviour {
+
+	public MonoBehaviour[] bootList = new MonoBehaviour[0];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
